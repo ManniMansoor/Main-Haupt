accd153c6575d0096083e2c68865c15761ac37b9
ManniMansoor <mansoor.ahmad579@gmail.com>
2026-04-06 21:13:47 +0200

    Initialize jirivyhnalek/react-fluentui-examples

---
+++ packages/react-data-grid-react-window/src/components/DataGridBody/renderDataGridBody.tsx
@@ -0,0 +1,31 @@
+import * as React from 'react';
+import { getSlots, useFluent } from '@fluentui/react-components';
+import type {
+  DataGridBodyState,
+  DataGridBodySlots,
+} from './DataGridBody.types';
+import { FixedSizeList as List } from 'react-window';
+
+/**
+ * Render the final JSX of DataGridVirtualizedBody
+ */
+export const renderDataGridBody_unstable = (state: DataGridBodyState) => {
+  const { slots, slotProps } = getSlots<DataGridBodySlots>(state);
+  const { dir } = useFluent();
+
+  return (
+    <slots.root {...slotProps.root}>
+      <List
+        itemSize={state.itemSize}
+        width={state.width}
+        itemData={state.rows}
+        height={state.height}
+        itemCount={state.rows.length}
+        direction={dir}
+        {...state.listProps}
+      >
+        {state.virtualizedRow}
+      </List>
+    </slots.root>
+  );
+};
