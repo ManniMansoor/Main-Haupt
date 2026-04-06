bb9503d71bec42dd05ab072180c3704704c80964
ManniMansoor <mansoor.ahmad579@gmail.com>
2026-04-06 21:14:07 +0200

    Initialize olkatruk/cap-theme/label

---
+++ packages/react-cap-theme/src/index.ts
@@ -0,0 +1,13 @@
+export { CAPThemeProvider } from './theme/CAPThemeProvider';
+export {
+  CAP_THEME_ONE_DRIVE,
+  CAP_THEME_SHAREPOINT,
+  CAP_THEME_TEAMS,
+} from './theme/CAPTheme';
+export { Button } from './components/Button/Button';
+export type {
+  ButtonProps,
+  ButtonState,
+} from './components/Button/Button.types';
+export { Label } from './components/Label/Label';
+export type { LabelProps, LabelState } from './components/Label/Label.types';
